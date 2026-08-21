65488452343ca3a166c83d58ef2a64e561ed44d8
louie-go <louie.decierdo@gmail.com>
2026-08-21 23:19:56 +0800

    added `stdlib.h` for `malloc` and changed fen to starting pos in `main.c`

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "bitboard.h"
 #include "board.h"
@@ -8,8 +9,8 @@
 #include "types.h"
 
 int main(void) {
-  Board *board = calloc(1, sizeof(*board));
-  parse_fen("8/8/8/3k4/8/8/4K3/R7 w - - 0 1", board);
+  Board *board = malloc(sizeof(*board));
+  parse_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", board);
   print_board(board, stdout);
 
   return 0;
