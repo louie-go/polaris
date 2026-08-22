27b8ca88f530113e9e173563af0ded5c2440afcf
louie-go <louie.decierdo@gmail.com>
2026-08-22 17:12:00 +0800

    added `Piece pieces[64]` field to `Board`, extra info on usage of `occupancies`, and commented out redundant `hash` field

--- src/board.h
+++ src/board.h
@@ -8,8 +8,11 @@
 typedef struct {
   Move         history[1024];
   Bitboard     bitboards[N_COLORS][N_PIECES];
+  Piece        pieces[64];
+  // +1 for `ALL` (check `types.h`)
   Bitboard     occupancies[N_COLORS+1];
-  uint64_t     hash; // TODO: implement zobrist hashing
+  // TODO: implement zobrist hashing
+  // uint64_t     hash;
   uint16_t     fullmove_no;
   uint8_t      halfmove_clk;
   Color        turn;
