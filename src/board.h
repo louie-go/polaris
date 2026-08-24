c8f30d18a98bd42dc283bc7bfdc006a756f0286b
louie-go <louie.decierdo@gmail.com>
2026-08-24 19:58:10 +0800

    change struct `Board` and added new struct `State`

--- src/board.h
+++ src/board.h
@@ -1,19 +1,19 @@
 #pragma once
 
-#include <stdio.h>
-
 #include "types.h"
 #include "bitboard.h"
 
+typedef struct State {
+  struct State *previous;
+  uint8_t      halfmove_clk;
+} State;
+
 typedef struct {
-  // TODO: implement PGN formatting
-  // Move         history[1024];
   Bitboard     bitboards[COLOR_LEN][PIECETYPE_LEN];
   Piece        pieces[64];
   // +1 for `ALL` (check `types.h`)
   Bitboard     occupancies[COLOR_LEN+1];
-  // TODO: implement zobrist hashing
-  // uint64_t     hash;
+  State        *state;
   uint16_t     fullmove_no;
   uint8_t      halfmove_clk;
   Color        turn;
@@ -22,6 +22,5 @@ typedef struct {
 } Board;
 
 void make_move(Board *, Move);
-// TODO: implement states
-// void undo_move(Board *);
+void undo_move(Board *);
 void print_board(const Board *, FILE *);
