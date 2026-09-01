25a78de174afbf9c08d9f0725fe7f7f6d7250dac
louie-go <louie.decierdo@gmail.com>
2026-09-01 22:11:53 +0800

    removed zobrist, planning to make it builtin to `struct Board`

--- src/main.c
+++ src/main.c
@@ -8,10 +8,8 @@
 #include "movegen.h"
 #include "parsing.h"
 #include "types.h"
-#include "zobrist.h"
 
 int main(void) {
-  init_zobrist(0);
   Board *board = malloc(sizeof(*board));
 
   parse_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", board);
