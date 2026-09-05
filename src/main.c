c403e7649412e2d2c506e06085dfcf16a6bfa7b6
louie-go <louie.decierdo@gmail.com>
2026-09-05 13:19:41 +0800

    added perft to `src/main.c` and fixed some bugs in movegen

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
+#include <inttypes.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "bitboard.h"
 #include "board.h"
@@ -8,20 +11,40 @@
 #include "parsing.h"
 #include "types.h"
 
+#define DEPTH 6
+
+uint64_t perft(uint8_t depth, Board *board) {
+  if (depth == 0) return 1;
+  uint64_t nodes = 0;
+
+  Move moves[MAX_MOVES];
+  Move *moves_end = legal_moves(board, moves);
+
+  char move_str[6];
+  for (Move *move = moves; move < moves_end; move++) {
+    make_move(board, *move);
+    uint64_t move_nodes = perft(depth-1, board);
+    nodes += move_nodes;
+
+    if (depth == DEPTH) {
+      format_move(*move, move_str);
+      printf("move %s nodes %" PRIu64 "\n",
+        move_str, move_nodes);
+    }
+
+    undo_move(board);
+  }
+
+  return nodes;
+}
+
 int main(void) {
   Board *board = malloc(sizeof(*board));
   parse_fen(STARTING_POSITION, board);
 
   print_board(board, stdout);
 
-  Move moves[MAX_MOVES];
-  Move *moves_end = legal_moves(board, moves);
-
-  char move_str[MAX_MOVE_BUFFER];
-  for (Move *move = moves; move < moves_end; move++) {
-    format_move(*move, move_str);
-    puts(move_str);
-  }
+  printf("nodes %" PRIu64 "\n", perft(DEPTH, board));
 
   free(board);
   return 0;
